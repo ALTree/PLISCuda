301922bed153affb7337aef25fea9177d0401e6a
Alberto Donizetti <alb.donizetti@gmail.com>
2015-10-20 14:01:34 +0200

    nsm/nsm.cu: implement get min value in tau vector

--- nsm/include/cuda/nsm.cuh
+++ nsm/include/cuda/nsm.cuh
@@ -4,11 +4,13 @@
 #include <cuda_runtime.h>
 #include <curand_kernel.h>
 #include <thrust/device_vector.h>
+#include <thrust/extrema.h>
 
 
 __global__ void fill_tau_array(float * tau, int sbc);
 
 void h_fill_tau_array(thrust::device_vector<float> &tau);
+int h_get_min_tau(thrust::device_vector<float> &tau);
 
 
 #endif /* NSM_CUH_ */
--- nsm/src/cuda/nsm.cu
+++ nsm/src/cuda/nsm.cu
@@ -17,3 +17,9 @@ void h_fill_tau_array(thrust::device_vector<float> &tau)
 {
 	fill_tau_array<<<1, tau.size()>>>(thrust::raw_pointer_cast(tau.data()), tau.size());
 }
+
+int h_get_min_tau(thrust::device_vector<float> &tau)
+{
+	thrust::device_vector<float>::iterator iter = thrust::min_element(tau.begin(), tau.end());
+	return iter - tau.begin();
+}
