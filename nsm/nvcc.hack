36597a6d9d345baa48cb1c81fb5e6b765184e926
Alberto Donizetti <alb.donizetti@gmail.com>
2016-03-31 14:16:02 +0200

    all: remove nsight stuff and unused files
    Former-commit-id: 6d7362a9b762686378be63f6b57cd23359ee441d
    Former-commit-id: 1683e79ca21e936bff7919fd8a4e0e822d77bee2

--- nsm/nvcc.hack
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-nvccPath=$(which nvcc)
-$nvccPath $* -std=c++11
--- nsm/test/cuda/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "cuda_tests.cuh"
-
-int main()
-{
-	printf("---------- start CUDA tests ----------\n\n");
-	run_rates_tests();
-	run_rand_tests();
-	printf("\n----------  end  CUDA tests ----------\n");
-}
