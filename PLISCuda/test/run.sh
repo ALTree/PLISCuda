84c7e9df814c88ba20ba2edfbc3db1e620688ad1
Alberto Donizetti <a.donizetti@cineca.it>
2016-09-12 12:23:45 +0200

    test: add diff-5125 system
    Also implement timing of tests.
    
    Updates #35

--- PLISCuda/test/run.sh
+++ PLISCuda/test/run.sh
@@ -2,20 +2,22 @@
 
 PLISCUDA="/galileo/home/userinternal/adonizet/progetto-PLISCuda/PLISCuda/PLISCuda/pliscuda"
 
-
 # run tests on system contained in folder $1
 function run_tests {
-	echo -e "#" $1 "#\n"
+	echo -e "====" $1 "====\n"
 
-	# run simulation
+	echo -n "  Running simulation.. "
 	cd $1
 	$PLISCUDA conf.txt > /dev/null
+	echo -e "done.\n"
 
-	echo -n "  populations test.. "
+	echo -n "  == populations test.. "
 	pop_test
 
 	# cleanup
+	echo -e ""
 	rm sim*
+	cd ..
 }
 
 # count global popolation of each specie in the simulation output
@@ -24,12 +26,13 @@ function pop_test {
 	AWKSUM='{for (i=1;i<=NF;i++) a[i]+=$i} END {for (i=1;i<=NF;i++) printf a[i]; printf "\n"}'
 	P1=$(tail -n +5 sim* | awk "$AWKSUM")
 	P2=$(cat golden/pops.txt)
+
 	if [ "$P1" == "$P2" ]; then
-		echo "PASS!"
+		echo "PASS."
 	else
 		echo "FAIL!"
-		echo "    want: $P2"
-		echo "    got:  $P1"
+		echo "    want: |$P2|"
+		echo "    got:  |$P1|"
 	fi
 }
 
@@ -40,7 +43,10 @@ echo -e "\n### Running PLISCuda tests ###\n"
 
 for D in *; do
     if [ -d "${D}" ]; then
-        run_tests ${D}
+        s=`date +%s%N`
+		run_tests ${D}
+		e=`date +%s%N`
+		echo -e "  Done ("$( echo -e "scale=4; ($e - $s)/1000000000" | bc -l ) "s)\n"
     fi
 done
 
