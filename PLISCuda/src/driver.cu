f819ae3fcbb72d5428e4f1405d4a3f1303b680ad
Alberto Donizetti <a.donizetti@cineca.it>
2016-09-22 11:44:49 +0200

    driver.cu: reorganize init section
    Fixes #70

--- PLISCuda/src/driver.cu
+++ PLISCuda/src/driver.cu
@@ -12,20 +12,25 @@ namespace PLISCuda {
 	void run_simulation(Topology t, State s, Reactions r, float * h_rrc, float * h_drc, float endTime,
 						int compartments_count, int * subv_constants, float log_freq)
 	{
+		// the number of subvolumes
 		unsigned int sbc = t.getN();
-		int spc = s.getS();
-		int rc = r.getR();
-
-		// sbc rounded up to the nearest multiple of 4
+		// sbc rounded up to the nearest multiple of 4 to shut up
+		// initcheck about uninitialized memory accesses.
 		unsigned int sbc4 = (sbc % 4 ? sbc+(4-sbc%4) : sbc);
 
+		int spc = s.getS();   // number of species
+		int rc = r.getR();    // number of reactions
+
 		std::cout << "\n-- Starting Simulation -- \n\n";
-	
+
+		// We use 512 threads for block, except when we have just 1
+		// block; in that case the number of threads will be equal to
+		// the number of subvolumes rounded up to a multiple of 32.
 		int threads = 512;
 		int blocks = ceil(sbc / 512.0);
-		if (blocks == 1) {
+		if (blocks == 1)
 			threads = ((sbc / 32) + 1)*32;
-		}
+
 		std::cout << "  [using " << threads << " threads and " << blocks << " block(s)]\n\n";
 
 		int nc = 10;             // threshold for critical/non-critical event
@@ -34,7 +39,7 @@ namespace PLISCuda {
 
 		std::cout << "  moving constants to Device...\n";
 
-		// move constants to GPU
+		// move constants to the GPU
 		gpuErrchk(cudaMemcpyToSymbol(SBC, &sbc, sizeof(unsigned int)));
 		gpuErrchk(cudaMemcpyToSymbol(SPC, &spc, sizeof(int)));
 		gpuErrchk(cudaMemcpyToSymbol(RC, &rc, sizeof(int)));
@@ -43,23 +48,41 @@ namespace PLISCuda {
 
 		std::cout << "  allocating memory on Device...\n";
 		std::cout.precision(3);
-	
-		// ----- allocate and memcpy state arrays -----
-		int * h_state = s.getState();
 
+
+	
+		// -------------------- state --------------------
+		int * h_state = s.getState();
 		int * d_state_curr;
 		int * d_state_next;
 		gpuErrchk(cudaMalloc(&d_state_curr, sbc4 * spc * sizeof(int)));
 		gpuErrchk(cudaMalloc(&d_state_next, sbc4 * spc * sizeof(int)));
+
+		// even if we allocated sbc4-long arrays, we must memcpy
+		// *exactly* sbc values, or we'll hit uninitialized memory on
+		// the CPU!
 		gpuErrchk(cudaMemcpy(d_state_curr, h_state, sbc * spc * sizeof(int), cudaMemcpyHostToDevice));
+
+		// state.next needs to be initialized to the old state
 		gpuErrchk(cudaMemcpy(d_state_next, d_state_curr, sbc * spc * sizeof(int), cudaMemcpyDeviceToDevice));
+		
+		state state = {
+			d_state_curr,
+			d_state_next
+		};
 
-		std::cout << "    system state requires     " << (2*sbc*spc*sizeof(int)) / (1024.0 * 1024.0) << " MB\n";
+		std::cout << "    system state requires     " 
+				  << (2*sbc*spc*sizeof(int))  // state (current and next)
+			/ (1024.0 * 1024.0) 
+				  << " MB\n";
+		// -----------------------------------------------
 
-		// ----- allocate and memcpy reactants and products arrays -----
+
+
+		// -------------------- reactions --------------------
+		// reactants and products
 		int * h_reactants = r.getReactants();
 		int * h_products = r.getProducts();
-
 		int * d_reactants;
 		int * d_products;
 		gpuErrchk(cudaMalloc(&d_reactants, spc * rc * sizeof(int)));
@@ -67,7 +90,12 @@ namespace PLISCuda {
 		gpuErrchk(cudaMemcpy(d_reactants, h_reactants, spc * rc * sizeof(int), cudaMemcpyHostToDevice));
 		gpuErrchk(cudaMemcpy(d_products, h_products, spc * rc * sizeof(int), cudaMemcpyHostToDevice));
 
-		// ----- allocate and memcpy rrc and drc -----
+		reactions reactions = {
+			d_reactants,
+			d_products,
+		};
+
+		// reactions and diffusion constants
 		float * d_rrc;
 		float * d_drc;
 		gpuErrchk(cudaMalloc(&d_rrc, rc * sizeof(float) * compartments_count));
@@ -75,79 +103,107 @@ namespace PLISCuda {
 		gpuErrchk(cudaMemcpy(d_rrc, h_rrc, rc * sizeof(float) * compartments_count, cudaMemcpyHostToDevice));
 		gpuErrchk(cudaMemcpy(d_drc, h_drc, spc * sizeof(float) * compartments_count, cudaMemcpyHostToDevice));
 
-		std::cout << "    reactions require         " <<
-			(2*spc*rc*sizeof(int) + (rc+spc)*sizeof(float) + (spc*sizeof(int))) / (1024.0 * 1024.0) << " MB\n";
+		// hors cache
+		int * d_hors;
+		gpuErrchk(cudaMalloc(&d_hors, spc * sizeof(int)));
+
+		// hors needs to be initialized immediately 
+		init_hors<<<1, 1>>>(d_hors, reactions, spc);
+
+		std::cout << "    reactions require         "
+				  << (2*spc*rc*sizeof(int) +    // reactants and products
+					  (rc+spc)*sizeof(float) +  // reactions and diffusion rates
+					  (spc*sizeof(int)))        // hors
+			/ (1024.0 * 1024.0)
+				  << " MB\n";
+		// -----------------------------------------------
 	
-		// ----- allocate and memcpy topology arrays -----
+
+
+		// -------------------- rates --------------------
+		float * d_rate_matrix;
+		float * d_react_rates_array;
+		float * d_diff_rates_array;
+		gpuErrchk(cudaMalloc(&d_rate_matrix, 3 * sbc * sizeof(float)));
+		gpuErrchk(cudaMalloc(&d_react_rates_array, sbc * rc * sizeof(float)));
+		gpuErrchk(cudaMalloc(&d_diff_rates_array, sbc * spc * sizeof(float)));
+		
+		rates rates = {
+			d_react_rates_array, 
+			d_diff_rates_array, 
+			d_rate_matrix,
+			d_rrc, d_drc
+		};
+
+		int * d_subv_consts;
+		gpuErrchk(cudaMalloc(&d_subv_consts, sbc4 * sizeof(int)));
+		gpuErrchk(cudaMemcpy(d_subv_consts, subv_constants, sbc * sizeof(int), cudaMemcpyHostToDevice));
+
+		std::cout << "    rates arrays require      " 
+				  << (sbc*(3+rc+spc)*sizeof(float)) 
+			/ (1024.0 * 1024.0) 
+				  << " MB\n";
+		// -----------------------------------------------
+
+		
+
+		// -------------------- topology --------------------
 		unsigned int * h_topology = t.getNeighboursArray();
-
 		unsigned int * d_topology;
-		gpuErrchk(cudaMalloc(&d_topology, 6 * sbc * sizeof(unsigned int)));
-		gpuErrchk(cudaMemcpy(d_topology, h_topology, 6 * sbc * sizeof(unsigned int), cudaMemcpyHostToDevice));
-
 		int * d_neighcount;
+		gpuErrchk(cudaMalloc(&d_topology, 6 * sbc * sizeof(unsigned int)));
 		gpuErrchk(cudaMalloc(&d_neighcount, sbc * sizeof(int)));
+		gpuErrchk(cudaMemcpy(d_topology, h_topology, 6 * sbc * sizeof(unsigned int), cudaMemcpyHostToDevice));
 
 		neigh neigh = {
 			d_topology,
 			d_neighcount
 		};
 
-
+		// neigh.count needs to be initialized immediately
 		init_ncount<<<blocks, threads>>>(neigh);
 
 		std::cout << "    system topology requires  " << (sbc*sizeof(int) + 
 														  6*sbc*sizeof(unsigned int)) / (1024.0 * 1024.0) 
 				  << " MB\n";
+		// --------------------------------------------------
 
-	
-		// ----- allocate rate matrix -----
-		float * d_rate_matrix;
-		gpuErrchk(cudaMalloc(&d_rate_matrix, 3 * sbc * sizeof(float)));
 
-		// ----- allocate and memcpy subv_constants array -----
-		int * d_subv_consts;
-		gpuErrchk(cudaMalloc(&d_subv_consts, sbc4 * sizeof(int)));
-		gpuErrchk(cudaMemcpy(d_subv_consts, subv_constants, sbc * sizeof(int), cudaMemcpyHostToDevice));
 
-		// ----- allocate react_rates and diff_rates array
-		float * d_react_rates_array;
-		float * d_diff_rates_array;
-		gpuErrchk(cudaMalloc(&d_react_rates_array, sbc * rc * sizeof(float)));
-		gpuErrchk(cudaMalloc(&d_diff_rates_array, sbc * spc * sizeof(float)));
-
-		std::cout << "    rates arrays require      " << (sbc*(3+rc+spc)*sizeof(float)) / (1024.0 * 1024.0) << " MB\n";
-
-		// ----- allocate tau thrust vector
+		// -------------------- tau --------------------
 		thrust::device_vector<float> tau(sbc4, INFINITY);
-
 		float * d_current_time;
 		float h_current_time = 0.0;
 		gpuErrchk(cudaMalloc(&d_current_time, sizeof(float)));
+		// ---------------------------------------------
 
-		// ----- allocate and initialize prng array
+
+
+		// -------------------- prng --------------------
 		curandStateMRG32k3a * d_prngstate;
 		gpuErrchk(cudaMalloc(&d_prngstate, sbc * sizeof(curandStateMRG32k3a)));
 		init_prng<<<blocks, threads>>>(d_prngstate);
 
-		std::cout << "    prng array require        " << (sbc*sizeof(curandStateMRG32k3a)) / (1024.0 * 1024.0) << " MB\n";
+		std::cout << "    prng array require        " 
+				  << (sbc*sizeof(curandStateMRG32k3a)) 
+			/ (1024.0 * 1024.0) 
+				  << " MB\n";
+		// ----------------------------------------------
 
-		// ----- allocate leap array
+
+
+		// -------------------- leap --------------------
 		char * d_leap;
 		gpuErrchk(cudaMalloc(&d_leap, sbc4 * sizeof(char)));
+		// -------------------- leap --------------------
 
-		// ----- allocate and initialize HORs array ------
-		int * d_hors;
-		gpuErrchk(cudaMalloc(&d_hors, spc * sizeof(int)));
 
-		reactions reactions = {
-			d_reactants,
-			d_products,
-		};
 
-		init_hors<<<1, 1>>>(d_hors, reactions, spc);
-
+		// -------------------- revert --------------------
 		thrust::device_vector<int> revert(sbc4, 0);
+		// ------------------------------------------------
+
+
 
 		// zero GPU memory, just to be sure
 		// TODO: remove(?) or check that we are zeroing everything
@@ -156,21 +212,11 @@ namespace PLISCuda {
 		gpuErrchk(cudaMemset(d_diff_rates_array, 0, sbc * spc * sizeof(float)));
 		gpuErrchk(cudaMemset(d_leap, SSA_FF, sbc4 * sizeof(char)));
 
+
+
 		std::cout.precision(5);
 
 		std::cout << "\n  computing initial rates...\n\n";
-
-		rates rates = {
-			d_react_rates_array, 
-			d_diff_rates_array, 
-			d_rate_matrix,
-			d_rrc, d_drc
-		};
-
-		state state = {
-			d_state_curr,
-			d_state_next
-		};
 
 		compute_rates<<<blocks, threads>>>(state, reactions, neigh, rates, d_subv_consts);
 
@@ -202,16 +248,14 @@ namespace PLISCuda {
 		std::clock_t sim_start = std::clock();
 
 
-
 #ifdef PROFILE
 		cudaProfilerStart();
 #endif
 
-
+		// ------------------------------ simulation loop start ------------------------------
 
 		int step = 0;
 		while(h_current_time < endTime) {
-
 			step++;
 		
 #ifdef LOG
@@ -221,7 +265,6 @@ namespace PLISCuda {
 			// get min tau from device
 			thrust::device_vector<float>::iterator iter = thrust::min_element(tau.begin(), tau.end());
 			int min_tau_sbi = iter - tau.begin();
-			
 			if (isinf(tau[min_tau_sbi]) || tau[min_tau_sbi] < 0.0) {
 				printf("\n -- WARNING: min(tau) = +Inf or < 0 - abort simulation --\n");
 				break;
@@ -248,7 +291,6 @@ namespace PLISCuda {
 			// check if we need to revert this step
 			check_state<<<blocks, threads>>>(state, thrust::raw_pointer_cast(revert.data()));
 			bool revert_state = !thrust::none_of(revert.begin(), revert.end(), thrust::identity<int>());
-
 			if(revert_state) {
 #ifdef LOG
 				std::cout << "\n -- WARNING: need to revert state --\n";
@@ -272,8 +314,8 @@ namespace PLISCuda {
 			} // end if(revert_state)
 
 			// If we're good (i.e. state.next has passed the
-			// check_state call), swap state.next with state.curr, 
-			// and then copy state.curr to next
+			// check_state call), swap state.next with state.curr, and
+			// then copy state.curr to next
 			int * temp = state.curr;
 			state.curr = state.next;
 			state.next = temp;
@@ -286,12 +328,9 @@ namespace PLISCuda {
 			compute_rates<<<blocks, threads>>>(state, reactions, neigh, rates, d_subv_consts);
 
 			// update tau array
-
 			compute_taus<<<blocks, threads>>>(state, reactions, d_hors, neigh, rates, 
 											  thrust::raw_pointer_cast(tau.data()), min_tau, 
 											  d_leap, d_prngstate);
-
-
 
 #ifdef LOG
 			gpuErrchk(cudaMemcpy(h_state, state.curr, sbc * spc * sizeof(int), cudaMemcpyDeviceToHost));
@@ -321,9 +360,9 @@ namespace PLISCuda {
 				log_file.close();
 			}
 #endif
-		
-		} // end while(h_current_time < endTime)
+		}
 
+		// ------------------------------ simulation loop end ------------------------------
 
 #ifdef PROFILE
 		cudaProfilerStop();
@@ -355,7 +394,7 @@ namespace PLISCuda {
 	} 
 
 	
-	// ----- utils functions for printing and other stuff -----
+	// -------------------- utils functions --------------------
   
 	void print_eltime(float secs)
 	{
@@ -417,9 +456,9 @@ namespace PLISCuda {
 	void print_leap_array(char * d_leap, int sbc)
 	{
 		std::cout << "--- [leap array] ---\n";
-		char * h_leap = new char[(sbc % 4 ? sbc+(4-sbc%4) : sbc)];
+		char * h_leap = new char[sbc];
 		std::vector<std::string> to_print = {"LEAP_CR", "LEAP_NOCR", "SSA", "SSA_FF"};
-		gpuErrchk(cudaMemcpy(h_leap, d_leap, (sbc % 4 ? sbc+(4-sbc%4) : sbc) * sizeof(char), cudaMemcpyDeviceToHost));
+		gpuErrchk(cudaMemcpy(h_leap, d_leap, sbc * sizeof(char), cudaMemcpyDeviceToHost));
 		for (int i = 0; i < sbc; i++) {
 			std::cout << "sbi " << i << "] " << "leap: " <<
 				to_print[h_leap[i] - LEAP_CR] << "\n";		
